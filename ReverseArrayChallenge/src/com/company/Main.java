0894fe67210aa51a07f3288f84b5dfb2ad850697
omaralzuhairi27 <omaralzuhairi27@gmail.com>
2019-07-02 11:28:40 +0200

    learning array

--- ReverseArrayChallenge/src/com/company/Main.java
+++ ReverseArrayChallenge/src/com/company/Main.java
@@ -1,8 +1,24 @@
 package com.company;
 
+import java.util.Arrays;
+
 public class Main {
 
-    public static void main(String[] args) {
-	// write your code here
+  public static void main(String[] args) {
+    int[] array = {1, 2, 3, 4, 5};
+    System.out.println(Arrays.toString(array));
+    revers(array);
+    System.out.println(Arrays.toString(array));
+
+  }
+
+  private static void revers(int[] array) {
+    int maxIndex = array.length - 1;
+    int halfIndex = array.length / 2;
+    for (int i = 0; i < halfIndex; i++) {
+      int temp = array[i];
+      array[i] = array[maxIndex - i];
+      array[maxIndex - i] = temp;
     }
+  }
 }
